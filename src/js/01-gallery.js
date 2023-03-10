c68a53b424de2f8e4d6ee5ca31943ac219f791e8
OlgaTikhonchik <olga882011@gmail.com>
2023-03-10 11:45:08 +0200

    Первое задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+           
+            alt="${description}"
+            />
+       </a>`;
+    })
+    .join('');
+}
+
+const lightboxModal = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
