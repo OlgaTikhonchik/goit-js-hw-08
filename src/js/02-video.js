1bc39bb9fcc5c45fe2c17d9b5574bb82bb5118a8
OlgaTikhonchik <olga882011@gmail.com>
2023-03-10 17:54:35 +0200

    Завершение работы

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const timeUpdate = function (time) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(time));
+};
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+const saveTime = localStorage.getItem(STORAGE_KEY);
+console.log(saveTime);
+const timeStop = JSON.parse(saveTime);
+console.log(timeStop.seconds);
+
+player.setCurrentTime(timeStop.seconds || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextInput, 500));
+
+populateText();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  console.log('Отправляем форму');
+  console.log(formData);
+  formData = {};
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateText() {
+  const saveData = localStorage.getItem(STORAGE_KEY);
+  if (saveData) {
+    formData = JSON.parse(saveData);
+  }
+
+  const entries = Object.entries(formData);
+
+  entries.forEach(function (el) {
+    const [key, value] = el;
+    form[key].value = value;
+  });
+}
